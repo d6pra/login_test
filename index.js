bac46d83b8cc8f16511c05c8c133f1718ca9d82f
d6pra <114367882+d6pra@users.noreply.github.com>
2022-10-04 20:44:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -12,16 +12,4 @@ function logar(){
 
         }
 
-        function logar(){
-
-            var login = document.getElementById('login').value;
-            var senha = document.getElementById('senha').value;
-
-            if(login == "trow" && senha == "trow"){
-                alert('Sucesso');
-                location.href = "home.html";
-            }else{
-                alert('Usuario ou senha incorretos');
-            }
-
-        }
+ 
